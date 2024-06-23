457ffb7826935f75264d7f76bc25b80fddba2fd8
Nivaldo Felizola <55410946+Nfelizola@users.noreply.github.com>
2024-06-23 02:33:20 -0300

    ok

--- js/codigo.js
+++ js/codigo.js
@@ -1,49 +1,71 @@
 // variaveis globais - carga de todas as selecoes
 // primeiro teste apenas grupo A
 // mas dveremos cadastrar todas as selecoes conforme exemplo...
-let times = [
-    { nome: "Alemanha", ranking: 1647.52, forca: 0, grupo: "A" },
-    { nome: "Escocia", ranking: 1461.55, forca: 0, grupo: "A" },
-    { nome: "Hungria", ranking: 1537.65, forca: 0, grupo: "A" },
-    { nome: "suiça", ranking: 1593.49,forca: 0, grupo: "A" },
-
-    { nome: "Espanha", ranking: 1715.22, forca: 0, grupo: "B" },
-    { nome: "Croácia", ranking: 1704.23, forca: 0, grupo: "B" },
-    { nome: "Itália", ranking: 1713.66, forca: 0, grupo: "B" },
-    { nome: "Albânia", ranking: 1351.53,forca: 0, grupo: "A" },
-
-    { nome: "Eslovênia", ranking: 1427.84, forca: 0, grupo: "C" },
-    { nome: "Dinamarca", ranking: 1602.72, forca: 0, grupo: "C" },
-    { nome: "Sérvia", ranking: 1496.53, forca: 0, grupo: "C" },
-    { nome: "Inglaterra", ranking: 1794.01,forca: 0, grupo: "C" },
-
-    { nome: "Países Baixos", ranking: 1731.13, forca: 0, grupo: "D" },
-    { nome: "França", ranking: 1840.59, forca: 0, grupo: "D" },
-    { nome: "Polônia", ranking: 1534.66, forca: 0, grupo: "D" },
-    { nome: "Áustria", ranking: 1546.12,forca: 0, grupo: "D" },
-
-    { nome: "Romênia", ranking: 1433.71, forca: 0, grupo: "E" },
-    { nome: "Bélgica", ranking: 1795.23, forca: 0, grupo: "E" },
-    { nome: "Eslováquia", ranking: 1427.84, forca: 0, grupo: "E" },
-    { nome: "Ucrânia", ranking: 1561.95,forca: 0, grupo: "E" },
-
-    { nome: "Portugal", ranking: 17433.71, forca: 0, grupo: "F" },
-    { nome: "Chéquia", ranking: 1482.12, forca: 0, grupo: "F" },
-    { nome: "Geórgia", ranking: 1287.43, forca: 0, grupo: "F" },
-    { nome: "Turquia", ranking: 1457.92,forca: 0, grupo: "F" },
-    
+let timea = [
+    { nome: "Argentina", ranking: 1843.73, forca: 0 },
+    { nome: "Chile", ranking: 1512.82, forca: 0 },
+    { nome: "Peru", ranking: 1553.23, forca: 0 },
+    { nome: "Canada", ranking: 1444.44,forca: 0},
 ];
+let timeb = [
+    { nome: "Equador", ranking: 1502.85, forca: 0 },
+    { nome: "Jamaica", ranking: 1435.33, forca: 0 },
+    { nome: "México", ranking: 1635.73, forca: 0 },
+    { nome: "Venezuela", ranking: 1442.29,forca: 0},
+];
+let timec = [
+    { nome: "Bolívia", ranking: 1283.88, forca: 0 },
+    { nome: "Est. Unidos", ranking: 1655.54, forca: 0 },
+    { nome: "Panamá", ranking: 1482.10, forca: 0 },
+    { nome: "Uruguai", ranking: 1637.55,forca: 0},
+];
+let timed = [
+    { nome: "Brasil", ranking: 1795.13, forca: 0 },
+    { nome: "Colômbia", ranking: 1612.78, forca: 0 },
+    { nome: "Costa Rica", ranking: 1440.77, forca: 0 },
+    { nome: "Paraguai", ranking: 1430.73,forca: 0},
+];
+let times = timea.concat(timeb,timec,timed);
 let gola=0;
 let golb=0;
-
+let menor=2000;
+// encontrar menor ranking
+times.forEach(rmenor);
 // gerar força baseado no fator 553.16
 times.forEach(criarforca);
+document.addEventListener('DOMContentLoaded', function() {
+    // Exemplo de manipulação do DOM
+    // iremos criar tofas as 
+// pegar o elemento referente ao grupoa pgrupoa
+let  Elemento = document.getElementById("grupoa");
+     
+if (document.title=="grupoa"){
+     alvo=timea;
+     criarpartidas(alvo,Elemento);
+  }
+  if (document.title=="grupob"){
+     alvo=timeb;
+     criarpartidas(alvo,Elemento);
+  }
+  if (document.title=="grupoc"){
+     alvo=timec;
+     criarpartidas(alvo,Elemento);
+  }
+  if (document.title=="grupod"){
+     alvo=timed;
+     criarpartidas(alvo,Elemento);
+  }
 
-// desta vez criando a função em separado para ficar mais visisvel
-// se quisermos poderiamos ter usado arrows function
+   
+    });
+function rmenor(item,index){
+  if (item.ranking<menor){
+    menor=item.ranking;
+  } 
+}
+
 function criarforca(item,index) {
-    item.forca= item.ranking/ 553.16;
-
+    item.forca= item.ranking/ menor;
 }
 
 // receberá a e b, sendo a e b os indices dos times na matriz de times
@@ -62,22 +84,22 @@ function partida(a,b) {
        // Fórmula seria:  (força * D10)+(sorte(D10)*dia(D2))
      let resultadoa, resultadob;
         // formula simplificada, menos sorte e mais ranking
-     resultadoa = (times[a].forca*D(6))+(D(6)*D(2));
-    resultadob = (times[b].forca*D(6))+(D(6)*D(2));
+     resultadoa = (alvo[a].forca*D(2))+(D(4)*D(2));
+    resultadob = (alvo[b].forca*D(2))+(D(4)*D(2));
     //console.log(times[a].nome + "Fez: "+ resultadoa);
     //console.log(times[b].nome + "Fez: "+resultadob);
    let mensagem="";
     if (resultadoa>resultadob){
-       mensagem = times[a].nome + "  have a oportuniite and...";
-        if (Math.round((resultadob/resultadoa)*100)<=70) {
+       mensagem = alvo[a].nome + "  have a oportuniite and...";
+        if (Math.round((resultadob/resultadoa))<=.70) {
             mensagem+="Gol....  aos "+espacomin*(n+1);
             gola++;
         }else{
             mensagem+="perdeu a oportunidade....  aos "+espacomin*(n+1);
         }
     }else {
-        mensagem =times[b].nome + "  have a oportuniite and...";
-        if (Math.round((resultadoa/resultadob)*100)<=70) {
+        mensagem =alvo[b].nome + "  have a oportuniite and...";
+        if (Math.round((resultadoa/resultadob))<=.70) {
             mensagem+="Gol.... aos "+espacomin*(n+1);
             golb++;
         }else{
@@ -88,7 +110,7 @@ function partida(a,b) {
     }
          
   console.log("Placar Final ");
-  console.log(times[a].nome +" "+gola+" X "+golb+" "+times[b].nome);
+  console.log(alvo[a].nome +" "+gola+" X "+golb+" "+alvo[b].nome);
   gola=0;
   golb=0;
   
@@ -101,3 +123,43 @@ function D(faces){
     return dado; 
 }
 
+
+   function criarpartidas(t,e){
+        // montar a pagina atraves da manipulação do DOM
+        // criar as combinações possíveis dentro do grupo 0,1  0,2  0,3 1,2, 1,3 2,3
+        //0,1
+        
+         e.innerHTML+='<img src="'+ t[0].nome+'.svg" class="bandeira">';
+        e.innerHTML+='<p>'+t[0].nome+'[_] x [_]'+t[1].nome+'</p>';
+        e.innerHTML+='<img src="'+t[1].nome+'.svg" class="bandeira">';
+        e.innerHTML+='<button id="start" class="botao" onclick="partida(0,1)">ok</button><p>45:00:00</p>';
+       //1,2
+       e.innerHTML+='<img src="'+ t[1].nome+'.svg" class="bandeira">';
+       e.innerHTML+='<p >'+t[1].nome+'[__]  x  [__]'+t[2].nome+'</p>';
+       e.innerHTML+='<img src="'+t[2].nome+'.svg" class="bandeira">';
+       e.innerHTML+='<button id="start" class="botao" onclick="partida(1,2)">ok</button><p>45:00:00</p>';
+       //0,3
+       e.innerHTML+='<img src="'+ t[0].nome+'.svg" class="bandeira">';
+       e.innerHTML+='<p>   '+t[0].nome+'[_] x [_]'+t[3].nome+'</p>';
+       e.innerHTML+='<img src="'+t[3].nome+'.svg" class="bandeira">';
+       e.innerHTML+='<button id="start" class="botao" onclick="partida(0,3)">ok</button><p>45:00:00</p>';
+      //1,3
+       e.innerHTML+='<img src="'+ t[1].nome+'.svg" class="bandeira">';
+        e.innerHTML+='<p>'+t[1].nome+'[__]  x  [__]'+t[3].nome+'</p>';
+        e.innerHTML+='<img src="'+t[3].nome+'.svg" class="bandeira">';
+        e.innerHTML+='<button id="start" class="botao" onclick="partida(1,3)">ok</button><p>45:00:00</p>';
+      //0,2
+      e.innerHTML+='<img src="'+ t[0].nome+'.svg" class="bandeira">';
+      e.innerHTML+='<p>'+t[0].nome+'[_] x [_]'+t[2].nome+'</p>';
+      e.innerHTML+='<img src="'+t[2].nome+'.svg" class="bandeira">';
+      e.innerHTML+='<button id="start" class="botao" onclick="partida(0,2)">ok</button><p>45:00:00</p>';
+        
+        //2,3
+        e.innerHTML+='<img src="'+ t[2].nome+'.svg" class="bandeira">';
+         e.innerHTML+='<p>'+t[2].nome+'[__]  x  [__]'+t[3].nome+'</p>';
+         e.innerHTML+='<img src="'+t[3].nome+'.svg" class="bandeira">';
+         e.innerHTML+='<button id="start" class="botao" onclick="partida(2,3)">ok</button><p>45:00:00</p>';
+                      
+       };
+
+       
